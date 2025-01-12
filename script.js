72af7cef16633a7d00d529cc7025b5b5032fce4d
Faris Abshiru <fabsheru77@gmail.com>
2025-01-11 22:36:11 -0800

    HW 1 - Tell me about yourself assignment has been Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Define variables for personal and academic details
+function displayDetails() {
+    const firstName = "Faris"; // First name
+    const lastName = "Abshiru"; // Last name
+    const yearsOfStudy = 3; // Number of years of study
+    const goal = "To be a software developer"; // Goal
+
+    // Display the values in the console
+
+    console.log(`First Name: ${firstName}`);
+    console.log(`Last Name: ${lastName}`);
+    console.log(`Years of Study: ${yearsOfStudy}`);
+    console.log(`Goal: ${goal}`);
+
+}
+// Call the function to display details
+displayDetails();
+
